d45c9e34e6f2ffc87023a470e9b579dc2d837efa
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-23 11:30:32 +0500

    Update DaysInMonth.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,13 @@
 package school.mjc.stage0.conditions.finalTask;
-
+import java.time.YearMonth;
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if(year < 1 || month > 12 || month < 1)
+        {
+            System.out.println("invalid date");
+            return;
+        }
+        System.out.println(YearMonth.of(year, month).lengthOfMonth());
 
     }
 }
